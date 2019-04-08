271e259a1479d98319b0575a2c8773bff11ab395
Syed Ahmad <syed-ahmad@outlook.com>
2019-04-08 13:11:28 +0100

    32. Redrecting Requests

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer(
-  // Register event listener
-  // Nodejs register single thread (behind the scenes it uses multi-threading)
-  (req, res) => {
-  console.log(req);
-  // Exit / kill event loop
-  process.exit();
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><titile>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.writeHead(302, { Location: '/'});
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
